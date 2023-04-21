d0ef304baae403c4b7e365e7d1166587de6eb65f
groundbreaking <mail@gdbg.tv>
2023-04-21 23:18:51 +0900

    IRの参照値修正

--- result/lua/valueproperty.lua
+++ result/lua/valueproperty.lua
@@ -21,10 +21,8 @@ module.value = {
   {id = "gauge", src = "parts", x = 816, y = 60, w = 341, h = 38, divx = 11, digit = 3, zeropadding = 3, ref = 107},
   {id = "gauge_dec", src = "parts", x = 816, y = 60, w = 310, h = 38, divx = 10, digit = 1, ref = 407},
 
-  -- {id = "ir_rank", src = "parts", x = 816, y = 98, w = 176, h = 19, divx = 11, digit = 4, zeropadding = 4, ref = 179},
-  -- {id = "ir_total", src = "parts", x = 816, y = 98, w = 176, h = 19, divx = 11, digit = 4, zeropadding = 4, ref = 180},
-  {id = "ir_rank", src = "parts", x = 816, y = 98, w = 176, h = 19, divx = 11, digit = 4, zeropadding = 4, ref = 151},
-  {id = "ir_total", src = "parts", x = 816, y = 98, w = 176, h = 19, divx = 11, digit = 4, zeropadding = 4, ref = 151},
+  {id = "ir_rank", src = "parts", x = 816, y = 98, w = 176, h = 19, divx = 11, digit = 4, zeropadding = 4, ref = 179},
+  {id = "ir_total", src = "parts", x = 816, y = 98, w = 176, h = 19, divx = 11, digit = 4, zeropadding = 4, ref = 180},
 
   {id = "ep",		src = "parts", x = 750, y = 792, w = 352, h = 37, divx = 11, digit = 4, ref = 420},
   {id = "epf",		src = "parts", x = 750, y = 755, w = 352, h = 37, divx = 11, digit = 4, ref = 421},
